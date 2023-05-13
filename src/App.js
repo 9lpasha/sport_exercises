3a646a9c956283255870e0b60915245c883a02cb
Павел Чураков <pavel.churakov@iss.digital>
2023-05-13 14:09:10 +0300

    all!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+
+const dataset = require('./assets/csvjson.json');
+console.log(dataset)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [page, setPage] = useState(1);
+    const [data, setData] = useState(dataset.slice(0, 20));
+
+    useEffect(() => {
+        setData(dataset.slice((page - 1) * 20, page * 20));
+    }, [page]);
+
+    return (
+        <div className="App">
+            <div style={{display: 'flex', width: '100%', justifyContent: 'center', gap: '20px'}}>
+                <button disabled={page === 1} onClick={() => setPage(page - 1)}>{"<"}</button>
+                <div>{page}</div>
+                <button disabled={page === 67} onClick={() => setPage(page + 1)}>{">"}</button>
+                Всего 67 страниц
+            </div>
+            <div>
+                {
+                    data.map(item => (
+                        <div style={{
+                            border: '2px solid black', padding: '5px', margin: '10px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}>
+                            <div style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'flex-start',
+                                width: 'fit-content'
+                            }}>
+                                <div>ID: <b>{item.id}</b></div>
+                                <div>Name: <b>{item.name}</b></div>
+                                <div>Target: <b>{item.target}</b></div>
+                                <div>Body part: <b>{item.bodyPart}</b></div>
+                                <div>Equipment: <b>{item.equipment}</b></div>
+                            </div>
+                            <img src={item.gifUrl} alt=""/>
+                        </div>
+                    ))
+                }
+            </div>
+            <div style={{display: 'flex', width: '100%', justifyContent: 'center', gap: '20px'}}>
+                <button disabled={page === 1} onClick={() => setPage(page - 1)}>{"<"}</button>
+                <div>{page}</div>
+                <button disabled={page === 67} onClick={() => setPage(page + 1)}>{">"}</button>
+                Всего 67 страниц
+            </div>
+        </div>
+    );
 }
 
 export default App;
